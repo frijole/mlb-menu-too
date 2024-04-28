82efef737491bd2c4138c4e10df353be170d3e93
Ian Meyer <ianmeyer@mac.com>
2024-04-28 13:17:19 -0400

    Initial Commit

---
+++ mlb-menu-too/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mlb-menu-too
+//
+//  Created by Ian on 4/28/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
