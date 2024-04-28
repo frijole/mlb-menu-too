84dda5e062f9ae6c51ef0a90fca4950b6e24c6ca
Ian Meyer <ianmeyer@mac.com>
2024-04-28 17:39:40 -0400

    view controller prototype

--- mlb-menu-too/ViewController.swift
+++ mlb-menu-too/ViewController.swift
@@ -3,13 +3,27 @@ import Cocoa
 class ViewController: NSViewController {
   @IBOutlet var tableView: NSTableView!
   
+  var loading: Bool = true {
+    didSet {
+      tableView.reloadData()
+      updateWindowSize()
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
   override func viewWillAppear() {
     super.viewWillAppear()
-    
+    updateWindowSize()
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+      self?.loading = false
+    }
+  }
+  
+  func updateWindowSize() {
     let contentSize = NSSize(width: view.frame.width, height: tableView.intrinsicContentSize.height + 8)
     view.window?.setContentSize(contentSize)
   }
@@ -24,13 +38,20 @@ class ViewController: NSViewController {
 
 extension ViewController: NSTableViewDataSource {
   func numberOfRows(in tableView: NSTableView) -> Int {
-    return 10
+    loading ? 1 : 10
   }
 }
 
 extension ViewController: NSTableViewDelegate {
   func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-    let identifier = NSUserInterfaceItemIdentifier("CellIdentifier")
+    if loading {
+      let identifier = NSUserInterfaceItemIdentifier("LoadingCell")
+      if let loadingView = tableView.makeView(withIdentifier: identifier, owner: nil) as? NSTableCellView {
+        return loadingView
+      }
+    }
+
+    let identifier = NSUserInterfaceItemIdentifier("TextCell")
     if let view = tableView.makeView(withIdentifier: identifier, owner: nil) as? NSTableCellView {
       view.textField?.stringValue = "Row \(row)"
       return view
